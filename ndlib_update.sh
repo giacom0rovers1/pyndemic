c94bb1f6b609f8b1eb04d70d9e8d434b7a1b7764
giacom0rovers1 <giacomo.roversi@student.unife.it>
2020-11-11 14:07:46 +0100

    Upgrades for remote calculation of random and lattice references

--- ndlib_update.sh
+++ ndlib_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/giacomo/anaconda3/lib/python3.8/site-packages/
+cd ~/anaconda3/lib/python3.8/site-packages/
 
 pip install --upgrade git+git://github.com/GiulioRossetti/ndlib.git
