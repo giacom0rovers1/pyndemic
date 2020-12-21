Average path length added, unconnected lockdown network fixed.

lockdown.py
@@ -20,7 +20,7 @@ import ndlib.models.epidemics as ep
 import ndlib.models.ModelConfig as mc
 # from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
 
-# import networkx as nx
+import networkx as nx
 import pyndemic as pn
 
 # import salience_unw as sl
@@ -50,13 +50,13 @@ def attack_list(graph, ranklist, thr):
     while graph.k_avg > thr:
         # remove node with highest rank until reaching an avg degree threshold
         graph.remove_node(nodes.pop())
-
+        
         # update average connectivity degree
         k = graph.degree()
         graph.degree_list = [d for n, d in k]
         graph.k_avg = np.mean(graph.degree_list)
-
-    return graph
+        
+    return graph.subgraph(max(nx.connected_components(graph), key=len)).copy()
 
 
 with open('pickle/network_realw.pkl', 'rb') as f:
@@ -67,7 +67,7 @@ with open('pickle/simulations_realw.pkl', 'rb') as f:
 
 
 Holme_lbc = pn.randnet("HK lockdown scenario",
-                       "lockHighBC",
+                       "lockHiBC_connected",
                        attack_list(Holme.G, Holme.G.BC_list, 12*redfa),
                        attack_list(Holme.Gmini, Holme.G.BC_list, 12*redfa))
 
@@ -386,13 +386,13 @@ lock.save()
 
 # %% fix
 
-with open('pickle/simulations_lockHighBC.pkl', 'rb') as f:
-    lock = pickle.load(f)
+# with open('pickle/simulations_lockHighBC.pkl', 'rb') as f:
+#     lock = pickle.load(f)
 
-lock.name = "HK lockdown scenario"
+# lock.name = "HK lockdown scenario"
 
-lock.Gmini = pn.graph_plots(lock.Gmini, lock.name, [0])
-lock.fig00 = lock.Gmini.fig0
+# lock.Gmini = pn.graph_plots(lock.Gmini, lock.name, [0])
+# lock.fig00 = lock.Gmini.fig0
 
-lock.G = pn.graph_plots(lock.G, lock.name, [1])
-lock.fig01 = lock.G.fig1
+# lock.G = pn.graph_plots(lock.G, lock.name, [1])
+# lock.fig01 = lock.G.fig1

pyndemic.py
@@ -82,7 +82,7 @@ def graph_tools(G):
     G.nn = len(list(G.nodes))
     G.nl = len(list(G.edges))
     
-    print("    Calculating connectivity...")
+    print("    [1/4] Calculating connectivity...")
     k = G.degree()
     G.degree_list = [d for n, d in k]
     G.k_avg = np.mean(G.degree_list)
@@ -91,9 +91,9 @@ def graph_tools(G):
     G.k_max = np.max(G.degree_list)  # G.degree_sequence[0]
     G.k_min = np.min(G.degree_list)  # G.degree_sequence[-1]
     G.k_histD = np.array(nx.degree_histogram(G))/G.nn
-    print("   Connectivity degree histogram completed.")
+    print("    Connectivity degree histogram completed.")
 
-    print("    Calculating betweenness centrality...")
+    print("    [2/4] Calculating betweenness centrality...")
     BC = nx.betweenness_centrality(G)  # , normalized = False)
     G.BC_list = [bc for bc in BC.values()]
     print("    Betweenness centrality list completed.")
@@ -109,13 +109,13 @@ def graph_tools(G):
     # # plt.spy(A)
     # G.eig_val, G.eig_vec = sp.sparse.linalg.eigs(G.A)
 
-    print("    Calculating clustering...")
+    print("    [3/4] Calculating clustering...")
     C = nx.clustering(G)
     G.C_list = [c for c in C.values()]
     G.C_avg = nx.average_clustering(G)
     print("    Clustering list completed.")
 
-    print("    Calculating shortest path lengths...")
+    print("    [4/4] Calculating shortest path lengths...")
     G.L_avg = nx.average_shortest_path_length(G)
     print("    Average shortest path lengt estimated.")
     